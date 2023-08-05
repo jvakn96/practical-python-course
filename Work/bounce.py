f4d2829a9e89da87ca96d3dd709bc98eb0f32559
jvakn96 <141506796+jvakn96@users.noreply.github.com>
2023-08-05 18:35:32 +0100

    n/a

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,8 @@
 # bounce.py
 #
 # Exercise 1.5
+ball_height = 100
+
+for i in range(10):
+    ball_height = ball_height * 0.6
+    print(round(ball_height,4))
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,22 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+extra_payment = 3684.11
+total_paid = 0.0
+months_required = 0.0
+
+for i in range(12):
+    principal = principal * (1+rate/12) - extra_payment
+    total_paid = total_paid + extra_payment
+    months_required = months_required + 1
+    
+while principal > 0:
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+    months_required = months_required + 1
+
+print('Total paid', round(total_paid,2), 'over ', months_required)
